783e704f2d50ff70a37dc16f996356e3b26b6a25
IgnBys <ignaatbyshnev@mail.ru>
2022-12-26 22:15:31 +0100

    implementation tg_bot

--- telegram_bot.py
+++ telegram_bot.py
@@ -1,5 +1,10 @@
 import telebot
 from telebot import types
+from tensorflow import keras
+import numpy as np
+import cv2
+
+model = keras.applications.VGG16()
 
 bot = telebot.TeleBot('5982274359:AAHBxZM7_42LBESOhsL_EnvDm_6b3GAWGOM')
 # @bot.message_handler(commands = ['start'])
@@ -11,23 +16,54 @@ bot = telebot.TeleBot('5982274359:AAHBxZM7_42LBESOhsL_EnvDm_6b3GAWGOM')
 
 
 
-# @bot.message_handler()
-# def all(message):
-#     if message.text == "hi":
-#         mess = f'Hello, <b>{message.from_user.first_name} </b>'
-#         bot.send_message(message.chat.id, mess, parse_mode='html')
-#     elif message.text == "info":
-#         mess = {message.from_user}
-#         bot.send_message(message.chat.id, mess, parse_mode='html')
-#     else:
-#         bot.send_message(message.chat.id, "Please, choose another command: \nhi\ninfo")
+@bot.message_handler()
+def all(message):
+    if message.text == "hi":
+        mess = f'Hello, <b>{message.from_user.first_name} </b>'
+        bot.send_message(message.chat.id, mess, parse_mode='html')
+    elif message.text == "info":
+        mess = {message.from_user}
+        bot.send_message(message.chat.id, mess, parse_mode='html')
+    elif message.text == "/help":
+        help(message)
+    elif message.text == "/start":
+        start(message)
+    elif message.text == "/statistics":
+        statistics(message)
+    else:
+        bot.send_message(message.chat.id, "Please, choose another command: \nhi\ninfo")
 #
 
+def CATorNOT(img_dir):
+    img = cv2.imread(img_dir)
+    img = cv2.resize(img, (224, 224))
+    img = np.array(img)
+    x = keras.applications.vgg16.preprocess_input(img)
+    x = np.expand_dims(x, axis=0)
 
+    res = model.predict(x)
+    res = np.argmax(res)
+
+    if 281 <= res <= 285:
+        return True
+    else:
+        return False
 
 @bot.message_handler(content_types=['photo'])
 def photo(message):
-    bot.send_message(message.chat.id, 'CAT EBANYI!')
+    print ('message.photo =', message.photo)
+    fileID = message.photo[-1].file_id
+    print ('fileID =', fileID)
+    file_info = bot.get_file(fileID)
+    print ('file.file_path =', file_info.file_path)
+    downloaded_file = bot.download_file(file_info.file_path)
+
+    with open("image.jpg", 'wb') as new_file:
+        new_file.write(downloaded_file)
+
+    img_dir = 'image.jpg'
+    bot.send_message(message.chat.id, CATorNOT(img_dir))
+
 
 @bot.message_handler(commands=['start'])
 def start(message):
@@ -36,7 +72,7 @@ def start(message):
                                       '- /help</b>', parse_mode = 'html')
 
 @bot.message_handler(commands=['statistics'])
-def start(message):
+def statistics(message):
     bot.send_message(message.chat.id, '<b>You haven\'t sent a photo yet</b>', parse_mode = 'html')
 
 @bot.message_handler(commands=['help'])
