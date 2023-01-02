126376099e526feeb010e63d123cbf012b1b5bd7
IgnBys <ignaatbyshnev@mail.ru>
2023-01-02 19:22:03 +0100

    all changes

--- telegram_bot.py
+++ telegram_bot.py
@@ -1,11 +1,6 @@
 import telebot
 from telebot import types
-# from tensorflow import keras
-# import numpy as np
-# import cv2
-#added comment
-# model = keras.applications.VGG16()
-# from always_false_recognizer import AlwaysFalseRecognizer
+
 from cat_recognizer import CatRecognizer
 
 bot = telebot.TeleBot('5982274359:AAHBxZM7_42LBESOhsL_EnvDm_6b3GAWGOM')
@@ -18,20 +13,6 @@ def download_photo(message):
     with open('image.jpg', 'wb') as new_file:
         new_file.write(downloaded_file)
 
-# def CATorNOT(img_dir):
-#     img = cv2.imread(img_dir)
-#     img = cv2.resize(img, (224, 224))
-#     img = np.array(img)
-#     x = keras.applications.vgg16.preprocess_input(img)
-#     x = np.expand_dims(x, axis=0)
-#
-#     res = model.predict(x)
-#     res = np.argmax(res)
-#
-#     if 281 <= res <= 285:
-#         return True
-#     else:
-#         return False
 
 class AlwaysFalseRecognizer(CatRecognizer):
     def is_cat(self, image_path: str) -> bool:
@@ -58,7 +39,7 @@ def statistics(message):
 
 @bot.message_handler(commands=['help'])
 def help(message):
-    bot.send_message(message.chat.id, open('myfile.txt', 'r', -1, 'utf-8').read(), parse_mode = 'html')
+    bot.send_message(message.chat.id, open('help_cmd_text.txt', 'r', -1, 'utf-8').read(), parse_mode ='html')
 
 @bot.message_handler()
 def all(message):
@@ -68,12 +49,6 @@ def all(message):
     elif message.text == 'info':
         mess = {message.from_user}
         bot.send_message(message.chat.id, mess, parse_mode='html')
-    elif message.text == '/help':
-        help(message)
-    elif message.text == '/start':
-        start(message)
-    elif message.text == '/statistics':
-        statistics(message)
     else:
         bot.send_message(message.chat.id, 'Please, choose another command: \nhi\ninfo')
 
