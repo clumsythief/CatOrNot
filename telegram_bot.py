df2536af46de20b4f557758ae340f619d3cf1d12
IgnBys <ignaatbyshnev@mail.ru>
2023-01-06 00:29:03 +0100

    threadings

--- telegram_bot.py
+++ telegram_bot.py
@@ -1,3 +1,6 @@
+import os
+import threading
+
 import telebot
 from telebot import types
 
@@ -10,9 +13,11 @@ def download_photo(message):
     fileID = message.photo[-1].file_id
     file_info = bot.get_file(fileID)
     downloaded_file = bot.download_file(file_info.file_path)
-    with open('image.jpg', 'wb') as new_file:
+    src = 'photos/' + str(threading.get_ident()) + '.jpg'
+    print(threading.get_ident())
+    with open(src, 'wb') as new_file:
         new_file.write(downloaded_file)
-
+        return src
 
 class AlwaysFalseRecognizer(CatRecognizer):
     def is_cat(self, image_path: str) -> bool:
@@ -22,9 +27,9 @@ cat_recognizer = AlwaysFalseRecognizer()
 
 @bot.message_handler(content_types=['photo'])
 def photo(message):
-    download_photo(message)
-    file_name = 'image.jpg'
+    file_name = download_photo(message)
     bot.send_message(message.chat.id, cat_recognizer.is_cat(file_name))
+    os.remove(file_name)
 
 
 @bot.message_handler(commands=['start'])
